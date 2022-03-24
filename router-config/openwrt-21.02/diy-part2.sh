e14cb84919059c7d64028a2e7b5aed651ddb6936
J_88 <44608302+Jbt88@users.noreply.github.com>
2022-03-24 12:42:12 +0700

    Update diy-part2.sh

--- router-config/openwrt-21.02/diy-part2.sh
+++ router-config/openwrt-21.02/diy-part2.sh
@@ -21,6 +21,12 @@ echo "DISTRIB_SOURCECODE='openwrt'" >>package/base-files/files/etc/openwrt_relea
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
 
+# change ssid
+sed -i "s/OpenWrt/FynnWrt/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/iw /ipconfig /g" package/kernel/mac80211/files/lib/netifd/wireless/mac80211.sh
+
+# change hostname
+sed -i "s/OpenWrt/FynnWrt/g" package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
@@ -31,6 +37,36 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk package/luci-app-amlogic
 
 # Add p7zip
 svn co https://github.com/hubutui/p7zip-lede/trunk package/lean/p7zip
+
+# Add luci-app-3ginfo-lite
+svn co https://github.com/4IceG/luci-app-3ginfo-lite/trunk package/luci-app-3ginfo-lite
+
+# Intel XMM 7650 LTE-A Pro modem.
+# svn co https://github.com/koshev-msk/xmm-modem/trunk package/xmm-modem
+
+# A Qos luci app filter device by MAC address
+svn co https://github.com/WROIATE/luci-app-mqos/trunk package/luci-app-mqos
+
+# luci-app-modeminfo
+svn co https://github.com/koshev-msk/luci-app-modeminfo/trunk package/luci-app-modeminfo
+
+# luci-app-smstools3 
+svn co https://github.com/koshev-msk/luci-app-smstools3/trunk package/luci-app-smstools3
+
+# luci-app-mqos
+svn co https://github.com/WROIATE/luci-app-mqos/trunk package/luci-app-mqos
+
+# Add luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
+rm -rf package/openwrt-passwall/{kcptun,xray-core} 2>/dev/null
+
+# Add luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash
+pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
+
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/depends/luci-lib-fs package/luci-lib-fs
 
 # coolsnowwolf default software package replaced with Lienol related software package
 # rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
